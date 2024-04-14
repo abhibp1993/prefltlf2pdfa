af0bfafbe991ede46ebf85ebbc2273ce683ead6c
Abhishek Kulkarni <abhi.bp1993@gmail.com>
2024-04-14 15:31:04 -0500

    restructure old files

--- wsgi.py
+++ wsgi.py
@@ -4,11 +4,9 @@ import sys
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 
-from app3 import server as app
-# from app2 import server as app
-# from app import server as app
-# from test_app import app as app
+from webapp import server as app
 from loguru import logger
+
 logger.add(os.path.join("home", "prefltlf", "webapp", "assets", "out", "app.log"), rotation="20 MB")
 # logger.info(sys.path)
 # logger.info(app.get_asset_url("app.log"))
