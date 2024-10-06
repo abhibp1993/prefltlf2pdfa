1277d83de10129207ab826a7c3e6d2d9767fc339
Abhishek Kulkarni <abhi.bp1993@gmail.com>
2024-10-05 20:17:29 -0500

    #update: docker & version

--- prefltlf2pdfa/__init__.py
+++ prefltlf2pdfa/__init__.py
@@ -1,3 +1,7 @@
 from prefltlf2pdfa.prefltlf import PrefLTLf, PrefAutomaton
 from prefltlf2pdfa.viz import paut2dot, paut2png, paut2svg, paut2base64
 from prefltlf2pdfa.semantics import *
+
+
+def version():
+    return "1.0"
